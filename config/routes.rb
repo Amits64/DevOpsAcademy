cbcfef31ddae578cc7909c5efa32a14f63c2473d
Amits64 <chauhanamit090@hotmail.com>
2024-08-10 10:57:35 +0000

    updates baked

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  # Route for the welcome page
+  get 'welcome/index'
+
   # Route for listing all courses
   get 'courses', to: 'courses#index', as: 'courses'
 
   # Route for showing a specific course
   get 'courses/:id', to: 'courses#show', as: 'course'
 
-  # Root route if needed
-  root 'courses#index'
+  # Set root route to the welcome page
+  root 'welcome#index'
 end
