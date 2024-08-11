7f5ca10e07818a59bcc19fb8749f69c4d20b287c
Amits64 <chauhanamit090@hotmail.com>
2024-08-11 12:10:18 +0000

    updated codebase

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'courses', to: 'courses#index', as: 'courses'
   get 'courses/:id', to: 'courses#show', as: 'course'
+  get 'about', to: 'about#index'
+  get 'contact', to: 'contact#index'
   
   root 'welcome#index'
 end
