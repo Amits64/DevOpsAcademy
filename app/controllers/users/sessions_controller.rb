d66cc48f3a9dad735bc5517001c1ebb5a4a33e74
Amits64 <chauhanamit090@hotmail.com>
2024-08-11 20:00:52 +0000

    updated static workflow

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,20 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
   # Displays the form for users to sign in.
   def new
     super
   end
 
-  # POST /resource/sign_in
   # Signs the user in and creates a session.
   def create
     super
   end
 
-  # DELETE /resource/sign_out
   # Signs the user out and ends the session.
   def destroy
     super
   end
-
-  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
